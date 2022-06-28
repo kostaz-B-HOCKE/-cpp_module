04a9f217f121d8db8b61c2810f4f3c16dc568edf
Emma Abradolf <eabradol@at-m1.msk.21-school.ru>
2022-06-28 11:15:41 +0300

    0.0

---
+++ ex00/1/src/main.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include <ctype.h>
+#include <string>
+#include <locale>
+
+int
+	main(int argc, char const *argv[])
+{
+
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	
+}
